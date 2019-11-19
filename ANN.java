e62cd2b9aa225b1e30aa3d4c9aca6604829a45a6
Perkuns <31985858+Perkuns@users.noreply.github.com>
2019-11-19 15:05:12 +0000

    ANN
    ANN and ANN node classes, performs the calculations based on inputs and all that

--- ANN.java
+++ ANN.java
@@ -1,9 +1,10 @@
+import java.util.LinkedList;
+
 public class ANN {
 
-    private double[][][] weights;
     private double[] inputs;
-    private int nodes;
     private int layers;
+    private LinkedList<AnnNode[]> layer = new LinkedList();
 
     public double sigmoid(double x){
         double out;
@@ -29,32 +30,98 @@ public class ANN {
 
     public ANN(int n, int l, double[] inp){
         this.layers = l;
-        this.nodes = n;
-        if (inp.length != this.nodes){
-            throw new RuntimeException("input vector is not of appropriate size");
+        this.inputs = inp;
+        int size = (int) Math.round((Math.random()* (n-1)))+1;
+        AnnNode[] lrs = new AnnNode[size];
+        for (int i = 0; i < size; i++){
+            lrs[i] = new AnnNode(inp.length);
+            lrs[i].calcWeightedSum(inp);
         }
-        weights = new double[nodes][inp.length][layers];
-        for (int i = 0; i < nodes; i++){
-            for (int j = 0; j < inp.length; j++){
-                for (int k = 0; k < layers; k++){
-                    weights[i][j][k] = (double) Math.round(Math.random() * 10000d)/10000d;
+        this.layer.add(lrs);
+        for (int i = 1; i < l; i++){
+            size = (int) Math.round((Math.random()* (n-1)))+1;
+            lrs = new AnnNode[size];
+            for (int j = 0; j < size; j++){
+                lrs[j] = new AnnNode(this.layer.getLast().length);
+            }
+            layer.add(lrs);
+        }
+    }
+
+    public void calculate(int x){
+        double[] next = new double[layer.getFirst().length];
+        double[] temp;
+        for (int i = 0; i < next.length; i++){
+            switch (x){
+                case 0: {
+                    next[i] = nullFunc(this.layer.getFirst()[i].getWeightedSum());
+                    this.layer.getFirst()[i].setOut(next[i]);
+                }
+                case 1: {
+                    next[i] = hypTan(this.layer.getFirst()[i].getWeightedSum());
+                    this.layer.getFirst()[i].setOut(next[i]);
+                }
+                case 2: {
+                    next[i] = cos(this.layer.getFirst()[i].getWeightedSum());
+                    this.layer.getFirst()[i].setOut(next[i]);
+                }
+                case 3: {
+                    next[i] = hypTan(this.layer.getFirst()[i].getWeightedSum());
+                    this.layer.getFirst()[i].setOut(next[i]);
+                }
+                case 4: {
+                    next[i] = sigmoid(this.layer.getFirst()[i].getWeightedSum());
+                    this.layer.getFirst()[i].setOut(next[i]);
+                }
+                case 5: {
+                    next[i] = gauss(this.layer.getFirst()[i].getWeightedSum());
+                    this.layer.getFirst()[i].setOut(next[i]);
                 }
             }
         }
-    }
 
 
-    public double[][][] getWeights() {
-        return weights;
-    }
+        for (int i = 1; i < layers; i++){
+            temp = next;
+            next = new double[this.layer.get(i).length];
+            for (int j = 0; j < next.length; j++){
+                layer.get(i)[j].calcWeightedSum(temp);
+                switch (x){
+                    case 0: {
+                        next[j] = nullFunc(this.layer.get(i)[j].getWeightedSum());
+                        this.layer.getFirst()[i].setOut(next[j]);
+                    }
+                    case 1: {
+                        next[j] = hypTan(this.layer.get(i)[j].getWeightedSum());
+                        this.layer.getFirst()[i].setOut(next[j]);
+                    }
+                    case 2: {
+                        next[j] = cos(this.layer.get(i)[j].getWeightedSum());
+                        this.layer.getFirst()[i].setOut(next[j]);
+                    }
+                    case 3: {
+                        next[j] = hypTan(this.layer.get(i)[j].getWeightedSum());
+                        this.layer.getFirst()[i].setOut(next[j]);
+                    }
+                    case 4: {
+                        next[j] = sigmoid(this.layer.get(i)[j].getWeightedSum());
+                        this.layer.getFirst()[i].setOut(next[j]);
+                    }
+                    case 5: {
+                        next[j] = gauss(this.layer.get(i)[j].getWeightedSum());
+                        this.layer.getFirst()[i].setOut(next[j]);
+                    }
+                }
+            }
+        }
 
-    public int getNodes(){
-        return nodes;
     }
 
     public int getLayers() {
-        return layers;
+        return this.layers;
     }
 
-
+    public double[] getInputs() {
+        return inputs;
+    }
 }
