f9c3fc9eeee45b4bb320a35086105ea162d5ac68
Perkuns <31985858+Perkuns@users.noreply.github.com>
2019-11-19 15:06:17 +0000

    update on main
    showing how stuff works

--- main.java
+++ main.java
@@ -3,7 +3,8 @@ public class main {
     public static void main(String[] args){
         double[] in = {4, 7, 9, 6, 2};
         ANN a = new ANN(5, 5, in);
-
-
+        for (int i = 0; i < 6; i++){
+            a.calculate(i);
+        }
     }
 }
